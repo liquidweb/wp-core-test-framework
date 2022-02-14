c58a974b7bac107dbead29fd8e664068666bcb53
Jenny Rasmussen <jennifer.rasmussen@liquidweb.com>
2022-02-14 11:31:12 -0500

    undoing previous commit to follow proper branching

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -159,11 +159,11 @@ install_db() {
 
 	if ! [ -z $DB_HOSTNAME ] ; then
 		if [ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]; then
-			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT"
+			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
 		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
 			EXTRA=" --socket=$DB_SOCK_OR_PORT"
 		elif ! [ -z $DB_HOSTNAME ] ; then
-			EXTRA=" --host=$DB_HOSTNAME"
+			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
 		fi
 	fi
 
